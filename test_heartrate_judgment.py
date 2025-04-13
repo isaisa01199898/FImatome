f71542efd58d62e7d1a36f1b931f43a8216ffd3b
isaisa01199898 <isamikonoue0119@gmail.com>
2025-04-13 15:06:50 +0900

    Update test_heartrate_judgment.py

--- test_heartrate_judgment.py
+++ test_heartrate_judgment.py
@@ -2,10 +2,11 @@ import numpy as np
 import time
 import openai
 
-    ser = serial.Serial('com4', 9600)  # ポート名とボーレートを環境に合わせる
-    data = []
+ser = serial.Serial('com4', 9600)  # ポート名とボーレートを環境に合わせる
+data = []
+liking
 
-    while len(data) < 15:
+while len(data) < 15:
         line = ser.readline().decode('utf-8').strip()
         data.append(line)
 
@@ -30,16 +31,21 @@ while True:
         break
     elif  num >85 :
     judebox=super集中
-    break
+        break
 
-if judebox==集中:
-    #AIの返答をだす
-    if sukinakoto==??
- elif judebox==not集中:
+   if judebox==集中:
+       #AIの返答をだす
+       if liking==history:
+       #AIのコード
+       elif sukinakoto==science:
+   elif liking==not集中:
          #AIの返答をだす
-    if sukinakoto==??
- elif judebox==super集中:
+           if liking==history:
+         #AIのコード
+             elif liking==science:
+    elif judebox==super集中:
          #AIの返答をだす
-    if sukinakoto==??
- 
+       if liking==history:
+       #AIのコード
+       elif liking==science: 
     time.sleep(1)
